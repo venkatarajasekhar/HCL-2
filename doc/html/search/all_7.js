b751abce682ab9b2c352f7aabe1bc0911460be69
armstrhu <armstrhu@gmail.com>
2015-05-21 15:54:08 -0600

    Added heterostack class.

--- doc/html/search/all_7.js
+++ doc/html/search/all_7.js
@@ -4,6 +4,8 @@ var searchData=
   ['heterodeque_3c_20t_2c_20types_2e_2e_2e_3e',['heterodeque&lt; T, Types...&gt;',['../classheterogeneous_1_1heterodeque_3_01_t_00_01_types_8_8_8_4.html',1,'heterogeneous']]],
   ['heteroqueue_2ehpp',['heteroqueue.hpp',['../heteroqueue_8hpp.html',1,'']]],
   ['heteroqueue_3c_20t_2c_20types_2e_2e_2e_3e',['heteroqueue&lt; T, Types...&gt;',['../classheterogeneous_1_1heteroqueue_3_01_t_00_01_types_8_8_8_4.html',1,'heterogeneous']]],
+  ['heterostack_2ehpp',['heterostack.hpp',['../heterostack_8hpp.html',1,'']]],
+  ['heterostack_3c_20t_2c_20types_2e_2e_2e_3e',['heterostack&lt; T, Types...&gt;',['../classheterogeneous_1_1heterostack_3_01_t_00_01_types_8_8_8_4.html',1,'heterogeneous']]],
   ['heterovector_2ehpp',['heterovector.hpp',['../heterovector_8hpp.html',1,'']]],
   ['heterovector_3c_20t_2c_20types_2e_2e_2e_3e',['heterovector&lt; T, Types...&gt;',['../classheterogeneous_1_1heterovector_3_01_t_00_01_types_8_8_8_4.html',1,'heterogeneous']]]
 ];
--- doc/html/search/files_1.js
+++ doc/html/search/files_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['heterodeque_2ehpp',['heterodeque.hpp',['../heterodeque_8hpp.html',1,'']]],
   ['heteroqueue_2ehpp',['heteroqueue.hpp',['../heteroqueue_8hpp.html',1,'']]],
+  ['heterostack_2ehpp',['heterostack.hpp',['../heterostack_8hpp.html',1,'']]],
   ['heterovector_2ehpp',['heterovector.hpp',['../heterovector_8hpp.html',1,'']]]
 ];
